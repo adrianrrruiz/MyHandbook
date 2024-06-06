db9f4866819ccb265a25cd3d73714b0807d032d5
Adrian Ruiz <adrianruiz0208@gmail.com>
2024-06-05 20:21:34 -0500

    Initial commit

---
+++ template.cpp
@@ -0,0 +1,38 @@
+//By Adrian Ruiz
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+
+#define fori(i, a) for (int i=0; i<(a); i++)
+#define foreach(a,x) for (auto& a : x)
+
+#define len(x) (int)(x).size()
+#define pb push_back
+#define f first
+#define s second
+#define all(x) x.begin(), x.end()
+
+const char nl = '\n';
+
+void solve() {
+
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+
+    int T = 1;
+    cin >> T;
+    while(T--) {
+        solve();
+    }
+
+    return 0;
+}
