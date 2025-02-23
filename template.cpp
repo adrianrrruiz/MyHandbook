fda38f42e2892240f8ff4e43aad886ef24a24dbc
Adrian Ruiz <adrianruiz0208@gmail.com>
2025-02-23 18:16:14 -0500

    New version: template.cpp

--- template.cpp
+++ template.cpp
@@ -1,22 +1,24 @@
 //By Adrian Ruiz
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
 typedef long long ll;
-
 typedef vector<int> vi;
 typedef vector<ll> vl;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
 
-#define fori(i, a) for (int i=0; i<(a); i++)
-#define foreach(a,x) for (auto& a : x)
+#define fori(i, a, b) for (int i=(a); i<(b); i++)
+#define forn(a,x) for (auto& a : x)
 
 #define len(x) (int)(x).size()
 #define pb push_back
 #define f first
 #define s second
 #define all(x) x.begin(), x.end()
+
+#define fast_io ios::sync_with_stdio(0); cin.tie(0);
 
 const char nl = '\n';
 
@@ -25,7 +27,7 @@ void solve() {
 }
 
 int main() {
-    cin.tie(0)->sync_with_stdio(0);
+    fast_io;
     cin.exceptions(cin.failbit);
 
     int T = 1;
