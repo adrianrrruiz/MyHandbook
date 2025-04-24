6ac35c43ae98bd63a5c0d753de32766b07835cb3
Adrian Ruiz <adrianruiz0208@gmail.com>
2025-04-24 15:24:38 -0500

    New version: template.cpp(pairs, ckmin, ckmax)

--- template.cpp
+++ template.cpp
@@ -4,10 +4,14 @@
 using namespace std;
 
 typedef long long ll;
-typedef vector<int> vi;
-typedef vector<ll> vl;
+
 typedef pair<int,int> pii;
 typedef pair<ll,ll> pll;
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
 
 #define fori(i, a, b) for (int i=(a); i<(b); i++)
 #define forn(a,x) for (auto& a : x)
@@ -15,12 +19,17 @@ typedef pair<ll,ll> pll;
 
 #define len(x) (int)(x).size()
 #define pb push_back
+#define ins insert
 #define f first
 #define s second
+#define mp make_pair
 #define all(x) x.begin(), x.end()
 #define rall(x) x.rbegin(), x.rend()
 
-#define fast_io ios::sync_with_stdio(0); cin.tie(0);
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+#define fast_io ios::sync_with_stdio(0); cin.tie(0); cin.exceptions(cin.failbit);
 
 const char nl = '\n';
 
@@ -30,7 +39,6 @@ void solve() {
 
 int main() {
     fast_io;
-    cin.exceptions(cin.failbit);
 
     int T = 1;
     cin >> T;
