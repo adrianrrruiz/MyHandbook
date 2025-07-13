0815979aca371f06e4341738acd949ab578c0636
Adrian Ruiz <adrianruiz0208@gmail.com>
2025-07-12 22:21:36 -0500

    New version: template.cpp(debug)

--- template.cpp
+++ template.cpp
@@ -3,35 +3,82 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
+/*
+Compile with \texttt{-DDEBUG} flag.
+*/
 
-typedef pair<int,int> pii;
-typedef pair<ll,ll> pll;
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x);
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? ", " : ""), __print(i); cerr << "}";}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ", "; __print(x.second); cerr << '}';}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifdef DEBUG
+#define debug(x...) cerr << "\e[91m"<<__func__<<":"<<__LINE__<<" [" << #x << "] = ["; _print(x); cerr << "\e[39m" << endl;
+#else
+#define debug(x...)
+#endif
+
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
+
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pl;
 
 typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<ld> vd;
 typedef vector<ll> vl;
-typedef vector<pii> vpii;
-typedef vector<pll> vpll;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
 
-#define fori(i, a, b) for (int i=(a); i<(b); i++)
-#define forn(a,x) for (auto& a : x)
-#define fil(v) forn(a,v) cin>>a
+template<class T> using pq = priority_queue<T>;
+template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+#define foran(i, a, n) for (int i=(a); i<(n); i++)
+#define forn(i, n) foran (i, 0, n)
+#define trav(a, x) trav (auto &a : x)
+#define fil(v) trav(a,v) cin>>a
 
 #define len(x) (int)(x).size()
+#define mp make_pair
 #define pb push_back
-#define ins insert
 #define f first
 #define s second
-#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
 #define all(x) x.begin(), x.end()
 #define rall(x) x.rbegin(), x.rend()
+#define ins insert
 
 template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
 template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
 
 #define fast_io ios::sync_with_stdio(0); cin.tie(0); cin.exceptions(cin.failbit);
 
+const int MOD = 1000000007;
 const char nl = '\n';
+const int MX = 100001;
+const int INF = 1000000000;
 
 void solve() {
 
