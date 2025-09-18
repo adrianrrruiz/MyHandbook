41345cf80abb7b6fdaf82e4a7f034302e0b5db0a
Adrian Ruiz <111095677+adrianrrruiz@users.noreply.github.com>
2025-09-17 22:42:56 -0500

    Add initial template for LosPeUJota Team

---
+++ lospeujota_template.cpp
@@ -0,0 +1,39 @@
+// By LosPeUJota Team
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+
+#define foran(i, a, n) for (int i=(a); i<(n); i++)
+#define forn(i, n) foran (i, 0, n)
+#define trav(a, x) for (auto &a : x)
+
+#define len(x) (int)(x).size()
+#define pb push_back
+#define f first
+#define s second
+#define all(x) x.begin(), x.end()
+
+#define fast_io ios::sync_with_stdio(0); cin.tie(0); cin.exceptions(cin.failbit);
+
+const char nl = '\n';
+
+void solve() {
+
+}
+
+int main() {
+    fast_io;
+
+    int T = 1;
+    cin >> T;
+    while(T--) {
+        solve();
+    }
+
+    return 0;
+}
