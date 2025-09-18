c69221cb272d985cae3fa4ce794843a3be7a6b62
Adrian Ruiz <111095677+adrianrrruiz@users.noreply.github.com>
2025-09-18 08:33:01 -0500

    V2: Template for LosPeUJota Team

--- lospeujota_template.cpp
+++ lospeujota_template.cpp
@@ -8,26 +8,22 @@ typedef long long ll;
 typedef vector<int> vi;
 typedef vector<ll> vl;
 
-#define foran(i, a, n) for (int i=(a); i<(n); i++)
-#define forn(i, n) foran (i, 0, n)
 #define trav(a, x) for (auto &a : x)
 
 #define len(x) (int)(x).size()
 #define pb push_back
-#define f first
-#define s second
 #define all(x) x.begin(), x.end()
 
-#define fast_io ios::sync_with_stdio(0); cin.tie(0); cin.exceptions(cin.failbit);
-
-const char nl = '\n';
+#define endl '\n'
 
 void solve() {
 
 }
 
 int main() {
-    fast_io;
+    ios::sync_with_stdio(0);
+    cin.tie(0); 
+    cin.exceptions(cin.failbit);
 
     int T = 1;
     cin >> T;
